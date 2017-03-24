1963c70b6ad05f29d3f42862d62f959baf3b3aa6
John Need <john.need@gmail.com>
2017-03-24 06:58:34 -0400

    runtotstate

--- app/state-machine.js
+++ app/state-machine.js
@@ -2,7 +2,19 @@ _history = [{}];
 _currentState = {};
 
 function runStateTo(stateIndex){
+
+    function _runToState(index, state){
+        if (index === 0 ){
+            return state;
+        }
+        const historyIndex = stateIndex - index + 1;
+        const newState = Object.assign({}, state, _history[historyIndex]);
+        return _runToState(index - 1, newState);
+    }
+
     if(stateIndex >= _history.length ){
         throw error;
     }
+
+    return _runToState(stateIndex, _history[0]);
 }
